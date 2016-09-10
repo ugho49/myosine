a0470b33f4a2378c301fe01bdb5d36e148dab0bb
Ugho Stephan <stephan.ugho@gmail.com>
2016-09-11 01:46:08 +0200

    add googleAnalytics

--- resources/views/layouts/public.blade.php
+++ resources/views/layouts/public.blade.php
@@ -78,6 +78,17 @@
     <script src="{{URL::to('/')}}/bower_components/bootstrap-material-design/dist/js/ripples.min.js"></script>
     <script src="{{URL::to('/')}}/js/main.js"></script>
     <script src="{{URL::to('/')}}/js/navbar.js"></script>
+    
+    <script>
+        (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+                    (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+                m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+        })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+        ga('create', 'UA-48402317-3', 'auto');
+        ga('send', 'pageview');
+    </script>
+
     @yield('script')
 
     @if(Session::has('bandeauContent'))
