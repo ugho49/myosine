44fc5cc290a893c95d809abab7bfa79c1129c099
Ugho Stephan <stephan.ugho@gmail.com>
2016-09-11 10:44:42 +0200

    add middleware authenticated with root

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -45,6 +45,7 @@ class Kernel extends HttpKernel
      */
     protected $routeMiddleware = [
         'auth' => \App\Http\Middleware\Authenticate::class,
+        'auth.root' => \App\Http\Middleware\AuthenticateWithRootAuth::class,
         'auth.basic' => \Illuminate\Auth\Middleware\AuthenticateWithBasicAuth::class,
         'can' => \Illuminate\Foundation\Http\Middleware\Authorize::class,
         'guest' => \App\Http\Middleware\RedirectIfAuthenticated::class,
